0b762524d44a1f8c7b5c56428e1c9176b0a7823f
lee zg <598407521@qq.com>
2016-07-28 09:29:22 +0800

    Add files via upload

---
+++ SMTP/NSStream+SKPSMTPExtensions.h
@@ -0,0 +1,16 @@
+//
+//  NSStream+SKPSMTPExtensions.h
+//  SMTPSender
+//
+//  Created by wu xiaoming on 13-1-23.
+//  Copyright (c) 2013年 wu xiaoming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CFNetwork/CFNetwork.h>
+
+@interface NSStream (SKPSMTPExtensions)
+
++ (void)getStreamsToHostNamed:(NSString *)hostName port:(NSInteger)port inputStream:(NSInputStream **)inputStream outputStream:(NSOutputStream **)outputStream;
+
+@end
